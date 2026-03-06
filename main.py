e167251cf4eb8ededc5a8c293c7f188a28ffcf12
Matheus-Pieroni <mpieroni950@gmail.com>
2026-03-06 20:20:33 -0300

    Mediana Adicionada

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
+dados_quase = [10 , 20 , 30 , 40 , 50 , 15 , 25, 29, 39]
 
 def limpar_dados () :
     pass
@@ -7,8 +8,16 @@ def limpar_dados () :
 def calcular_media () :
     pass
 
-def calcular_mediana () :
-    pass
+def calcular_mediana(dados):
+    sorted(dados)
+    m = len(dados) / 2
+    print(m)
+    if not m.is_integer(): # Se esse valor não for um int
+        mCerto = int(m) #Ele transforma em int
+        print(mCerto)
+        result = (dados[mCerto] + dados[mCerto+1]) / 2 #calcula a media desses dois
+    else: result = m; pass
+    print(f"A mediana destes dados é, {result}")
 
 def calcular_variancia () :
     pass
@@ -16,6 +25,8 @@ def calcular_variancia () :
 def obter_extremos () :
     pass
 
-dados = limpar_dados ( dados_sujos )
+calcular_mediana(dados_quase)
+#dados = limpar_dados ( dados_sujos )
 
-print ( f" Dados processados : { dados }")
+#print ( f" Dados processados : { dados }")
+print(f"Arquivo auditado por: Matheus-Pieroni")
